00f65e1c1279d6470966c65d155362c788e0762f
Dmitrij <whynotmonkey@yandex.ru>
2016-03-04 23:46:31 +0200

    New changelist

--- app/src/main/java/com/example/dmitrij/remindme/MainActivity.java
+++ app/src/main/java/com/example/dmitrij/remindme/MainActivity.java
@@ -2,15 +2,17 @@ package com.example.dmitrij.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 
 
-public class MainActivity extends Activity{
+public class MainActivity extends AppCompatActivity{
 
     private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
