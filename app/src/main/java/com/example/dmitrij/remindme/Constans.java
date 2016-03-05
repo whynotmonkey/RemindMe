2578a8485f54d776fbe700f239daa7946167707a
Dmitrij <whynotmonkey@yandex.ru>
2016-03-05 14:58:41 +0200

    New changelist

---
+++ app/src/main/java/com/example/dmitrij/remindme/Constans.java
@@ -0,0 +1,10 @@
+package com.example.dmitrij.remindme;
+
+
+public class Constans {
+
+
+    public  static  final int TAB_ONE = 0;
+    public  static  final int TAB_TWO = 1;
+    public  static  final int TAB_THREE = 2;
+}
--- app/src/main/java/com/example/dmitrij/remindme/MainActivity.java
+++ app/src/main/java/com/example/dmitrij/remindme/MainActivity.java
@@ -1,24 +1,43 @@
 package com.example.dmitrij.remindme;
 
-import android.app.Activity;
+import android.app.Notification;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 
+import com.example.dmitrij.remindme.adapter.TabsPagerFragmentAdapter;
+
 
 public class MainActivity extends AppCompatActivity{
 
+
+    private static final int LAYOUT = R.layout.activity_main;
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ViewPager viewPager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
+        initTabs();
+
+
+
     }
-  private void initToolbar(){
+    private void initToolbar(){
       toolbar = (Toolbar) findViewById(R.id.toolbar);
       toolbar.setTitle(R.string.app_name);
       toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
@@ -29,5 +48,46 @@ public class MainActivity extends AppCompatActivity{
       });
       toolbar.inflateMenu(R.menu.menu);
 
+    }
+
+    private void initTabs(){
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLatout);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+
+    private void initNavigationView() {
+        drawerLayout =(DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.view_navigation_open, R.string.view_navigation_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView =(NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                switch (menuItem.getItemId()){
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                }
+                return false;
+
+
+
+                    
+                        
+                }
+        });
+
+    }
+    private  void showNotificationTab(){
+        viewPager.setCurrentItem(Constans.TAB_TWO);
     }
 }
